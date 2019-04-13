4391b14091dacea0ae166804160f885bc0063d5b
kziebinski1234 <konradziembinski1@gmail.com>
2019-04-13 13:23:24 +0200

    Pierwsza wersja kodu

--- KółkoKrzyzykXD/Program.cs
+++ KółkoKrzyzykXD/Program.cs
@@ -10,6 +10,21 @@ namespace KółkoKrzyzykXD
     {
         static void Main(string[] args)
         {
+            string imieGraczaA = "";
+            string imieGraczab = "";
+            char znakGraczaA = "X";
+            char znakGraczaB = "O";
+                char[,] plansza = new char[3, 3]
+                {
+                    { '1', '2', '3' },
+                    { '4', '5', '6' },
+                    { '7', '8', '9' }
+                };
+            Console.Write("Wpisz imie graczaA: ");
+            imieGraczaA = Console.ReadLine();
+            Console.Write("Wpisz imie graczaB: ");
+            imieGraczab = Console.ReadLine();
+            //...
         }
     }
 }
